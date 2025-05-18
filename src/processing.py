f4956d876b3c89ca00b6791d21d1ab207dd9a000
agaranina <aleksandra.belik74@gmail.com>
2025-05-18 12:44:42 +0700

    homework 10_1 fixed

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,20 @@
-def filter_by_state(data_base: list, state='EXECUTED') -> list:
+def filter_by_state(data_base: list, state: str = "EXECUTED") -> list:
     """
     Принимает список словарей и опционально значение для ключа state
     и возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению.
     """
     new_data_base = []
-    #Запускаем цикл для проверки статуса ключа state
+    # Запускаем цикл для проверки статуса ключа state
     for data in data_base:
-        if data['state'] == state:
+        if data["state"] == state:
             new_data_base.append(data)
         else:
             continue
     return new_data_base
 
 
-def sort_by_date(data_base: list, sort_by=True) -> list:
+def sort_by_date(data_base: list, sort_by: bool = True) -> list:
     """
     Принимает список словарей и необязательный параметр, задающий порядок сортировки
     (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный
@@ -22,6 +22,5 @@ def sort_by_date(data_base: list, sort_by=True) -> list:
     """
     sorted_date = []
     for data in data_base:
-        sorted_date = sorted(data_base, key=lambda data: data['date'], reverse=sort_by)
+        sorted_date = sorted(data_base, key=lambda data: data["date"], reverse=sort_by)
     return sorted_date
-
